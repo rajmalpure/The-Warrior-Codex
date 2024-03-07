d9f25ce42c1ae5077115e5d40edadf7523afddd9
rajmalpure <malpure.raj@kalvium.community>
2024-03-07 11:41:30 +0530

    M12

---
+++ server/schema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const warrior = new mongoose.Schema({
+        Warrior : String,
+        BirthYear : Number,
+        DeathYear : Number,
+        State : String,
+        FamousBattle : String,
+        Image : String
+})
+
+const war = mongoose.model('warriors-collections', warrior)
+
+module.exports = war
